99c246023591812393557e10af3d3d574fed439f
김한철 <gkscjf137@naver.com>
2024-12-02 20:11:36 +0900

    ㅇㅇ

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig {
                 registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                         .allowedOrigins(
                                 "http://localhost:3000", // 로컬 개발 서버
-                                "https://hancheolKim.github.io" // 배포된 리액트 앱 URL
+                                "https://hancheolKim.github.io", // 배포된 리액트 앱 URL
+                                "https://w5d72d166d2ed4d422a9bb5407dfb7fd8.apppaas.app" // 백엔드 배포 URL 추가
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용되는 HTTP 메서드
                         .allowCredentials(true); // 인증 정보 허용 (쿠키 등)
