0cce2432b72a2a667df9b915dfe308f60be0fac3
김한철 <gkscjf137@naver.com>
2024-12-13 19:05:06 +0900

    페이징 방식 변경

--- src/main/java/com/erp/controller/ItemController.java
+++ src/main/java/com/erp/controller/ItemController.java
@@ -35,45 +35,37 @@ public class ItemController {
                 keyword = URLDecoder.decode(keyword, StandardCharsets.UTF_8.name());
             }
         } catch (UnsupportedEncodingException e) {
-            System.out.println("Error decoding keyword: " + e.getMessage());
+            log.error("Error decoding keyword: {}", e.getMessage());
         }
 
         // 검색에 필요한 파라미터를 맵에 추가
         Map<String, Object> map = new HashMap<>();
         map.put("keyfield", keyfield);
         map.put("keyword", keyword);
-        System.out.println("Search map created: " + map);
 
         // 전체, 검색 레코드 수
         int count = itemService.getItemCount(map);
-        System.out.println("Total record count: " + count);
 
         // 페이지 처리
-        PagingUtil page = new PagingUtil(pageNum, count, 15, 10);
+        PagingUtil page = new PagingUtil(pageNum, count, 15, 10); // 한 페이지에 15개 아이템, 10개 페이지 버튼
 
         List<ItemVO> items = null;
         if (count > 0) {
-            System.out.println("Records found, preparing to fetch items.");
             map.put("order", order);
             map.put("start", page.getStartRow());
             map.put("end", page.getEndRow());
-            System.out.println("Map for fetching items: " + map);
 
             items = itemService.getAllItems(map);
-            System.out.println("Items fetched: " + items);
-        } else {
-            System.out.println("No records found.");
         }
 
         // 응답 데이터 구성
         Map<String, Object> response = new HashMap<>();
-        response.put("items", items);
-        response.put("page", page.getPage());
-        response.put("count", count);
-        System.out.println("Response data prepared: " + response);
+        response.put("items", items);           // 아이템 목록
+        response.put("pageHtml", page.getPageHtml()); // 생성된 페이지 버튼 HTML
+        response.put("count", count);           // 전체 아이템 수
+        response.put("pageNum", pageNum);      // 현재 페이지 번호
+        response.put("totalCount", (int) Math.ceil((double) count / 15)); // 전체 페이지 수
 
         return ResponseEntity.ok(response);
     }
-
-
 }
--- src/main/java/com/erp/util/PagingUtil.java
+++ src/main/java/com/erp/util/PagingUtil.java
@@ -3,7 +3,7 @@ package com.erp.util;
 public class PagingUtil {
 	private int startRow;    // 한 페이지에서 보여줄 게시글의 시작 번호
 	private int endRow;      // 한 페이지에서 보여줄 게시글의 끝 번호
-	private StringBuffer page; // 페이지 표시 문자열
+	private String pageHtml; // 페이지 버튼을 포함한 HTML 문자열
 
 	/**
 	 * count : 전체 게시물 수
@@ -27,7 +27,7 @@ public class PagingUtil {
 			endRow = currentPage * rowCount;
 
 			// 페이지 버튼 생성
-			page = new StringBuffer();
+			pageHtml = "";
 			if (pageCount > 0) {
 				// 시작 페이지와 마지막 페이지 값 구하기.
 				int startPage = (int) ((currentPage - 1) / pageCount) * pageCount + 1;
@@ -39,9 +39,7 @@ public class PagingUtil {
 
 				// 이전 block 페이지
 				if (currentPage > pageCount) {
-					page.append("<span class='pageButton' style='cursor:pointer;' onclick='goPage(" + (startPage - 1) + ")'>");
-					page.append("[이전]");
-					page.append("</span>");
+					pageHtml += "<span class='pageButton' style='cursor:pointer;' onclick='goPage(" + (startPage - 1) + ")'>[이전]</span>";
 				}
 				// 페이지 번호. 현재 페이지는 빨간색으로 강조하고 링크를 제거.
 				for (int i = startPage; i <= endPage; i++) {
@@ -49,30 +47,24 @@ public class PagingUtil {
 						break;
 					}
 					if (i == currentPage) {
-						page.append("&nbsp;<span class='pageButton' style='color:red;'>");
-						page.append(i);
-						page.append("</span>");
+						pageHtml += "&nbsp;<span class='pageButton' style='color:red;'>" + i + "</span>";
 					} else {
-						page.append("&nbsp;<span class='pageButton' style='cursor:pointer;' onclick='goPage(" + i + ")'>");
-						page.append(i);
-						page.append("</span>");
+						pageHtml += "&nbsp;<span class='pageButton' style='cursor:pointer;' onclick='goPage(" + i + ")'>" + i + "</span>";
 					}
-					page.append("&nbsp;");
+					pageHtml += "&nbsp;";
 				}
 				// 다음 block 페이지
 				if (totalPage - startPage >= pageCount) {
-					page.append("<span class='pageButton' style='cursor:pointer;' onclick='goPage(" + (endPage + 1) + ")'>");
-					page.append("[다음]");
-					page.append("</span>");
+					pageHtml += "<span class='pageButton' style='cursor:pointer;' onclick='goPage(" + (endPage + 1) + ")'>[다음]</span>";
 				}
 			} else {
-				page.append("<b>[warning]</b>pageCount는 1이상 지정해야 페이지수가 표시됩니다.");
+				pageHtml = "<b>[warning]</b>pageCount는 1이상 지정해야 페이지수가 표시됩니다.";
 			}
 		}
 	}
 
-	public StringBuffer getPage() {
-		return page;
+	public String getPageHtml() {
+		return pageHtml;
 	}
 
 	public int getStartRow() {
