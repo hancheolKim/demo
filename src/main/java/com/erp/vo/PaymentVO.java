39f66d01f7f50486502e76d76b30a6200ab73930
김한철 <gkscjf137@naver.com>
2024-12-16 16:56:11 +0900

    한번 제대로 결제해보자

--- src/main/java/com/erp/vo/PaymentVO.java
+++ src/main/java/com/erp/vo/PaymentVO.java
@@ -17,7 +17,7 @@ public class PaymentVO {
     private String itemNum;       // 상품 번호
     private int salesQuantity;    // 판매 수량
     private BigDecimal totalPrice; // 총 가격
-    private BigDecimal total_cost_price;
+    private BigDecimal totalCostPrice;
     private Date salesRegDate;    // 판매 등록 날짜
     private String payType;       // 결제 유형
     private int userNum;          // 사용자 번호
